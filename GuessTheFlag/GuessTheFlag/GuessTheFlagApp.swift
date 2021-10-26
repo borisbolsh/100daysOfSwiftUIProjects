8affe2eff27b2a2335ab7e56a7611ec243dbb55c
borisbolsh <bolsh.bo@yandex.ru>
2021-10-26 21:10:36 +0300

    2 Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,16 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
