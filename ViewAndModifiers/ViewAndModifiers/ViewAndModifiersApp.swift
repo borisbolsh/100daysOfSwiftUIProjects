88e45f5a7da8fb449c422c5ad0ca2a18b2064b62
borisbolsh <bolsh.bo@yandex.ru>
2021-10-27 23:18:54 +0300

    3 Project

---
+++ ViewAndModifiers/ViewAndModifiers/ViewAndModifiersApp.swift
@@ -0,0 +1,16 @@
+//
+//  ViewAndModifiersApp.swift
+//  ViewAndModifiers
+//
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
