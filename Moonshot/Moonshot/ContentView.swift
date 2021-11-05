c6bf527b57d898c906ad6830b53093f285ca40ce
borisbolsh <boris.bolsh@gmail.com>
2021-11-05 23:27:18 +0300

    Moonshot. part 2

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,9 +7,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
-        Text("\(astronauts.count)")
+            NavigationView {
+                List(missions) { mission in
+                    NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
+                        Image(mission.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 44, height: 44)
+
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                            Text(mission.formattedLaunchDate)
+                        }
+                    }
+                }
+                .navigationBarTitle("Moonshot")
+            }
     }
 }
 
