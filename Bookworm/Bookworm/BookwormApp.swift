d0a9321d92f55e18807d2bbd2c41a29907321407
borisbolsh <bolsh.bo@yandex.ru>
2021-11-08 18:55:05 +0300

    Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,19 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
